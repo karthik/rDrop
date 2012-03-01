ff277560ad8348808ec0af9bc5e7ebec1221babd
Karthik Ram <karthik.ram@gmail.com>
2012-02-29 23:35:35 -0800

    Minor formatting.

--- dropbox_share.r
+++ dropbox_share.r
@@ -1,7 +1,7 @@
 #Status: Works but needs error catching
 
 
-#' functions to share or possibly move a file to public folder and get the public url.
+#' Functions to share or possibly move a file to public folder and get the public url.
 #' Function to create a share for any file or folder and return a URL.
 #'@param cred Specifies an object of class ROAuth with Dropobox specific credentials.
 #'@param file Specifies the path to the file or folder you want a shareable link to.
@@ -19,12 +19,11 @@ if(!is.dropbox.cred(cred)) stop("Invalid Oauth credentials",call. = FALSE)
 # VERIFY THAT FILE DOES EXIST
 path_to_share <-paste("https://api.dropbox.com/1/shares/dropbox/",file,sep="")
 cred$OAuthRequest(path_to_share)
-
 # cred$OAuthRequest("https://api.dropbox.com/1/shares/dropbox/testing123")
 # result<-fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/shares/dropbox/testing123")
-	result<-fromJSON(cred$OAuthRequest(path_to_share))
+	result <- fromJSON(cred$OAuthRequest(path_to_share))
 	res <- list()
-	res$url <-result[[1]]
-	res$expires <-result[[2]]
+	res$url <- result[[1]]
+	res$expires <- result[[2]]
 	return(res)
 }
