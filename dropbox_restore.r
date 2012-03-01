a0dcce621f11b910436a5fd6bf468a0a43dd18ab
Karthik Ram <karthik.ram@gmail.com>
2012-03-01 12:25:27 -0800

    formatting

--- dropbox_restore.r
+++ dropbox_restore.r
@@ -1,6 +1,7 @@
 # Status: Still setting up
 
-# functions to share or possibly move a file to public folder and get the public url.
+# functions to share or possibly move a file to public folder and
+#   get the public url.
 #'
 #'@param cred Specifies an object of class ROAuth with Dropobox specific credentials.
 #'@param file  The path to the file.
@@ -12,12 +13,15 @@
 #'@examples \dontrun{
 #'
 #'}
-dropbox_restore<-function(cred,path,rev=NULL)
-{
-if(!is.dropbox.cred(cred)) stop("Invalid Oauth credentials",call. = FALSE)
-if(is.null(rev)) { stop ("You need to specify a revision number to restore a file \n",call.=F)}
-# List should contain path and revision number
-# 1. Check revision to make sure it exists.
+dropbox_restore <- function(cred, path, rev = NULL) {
+    if (!is.dropbox.cred(cred))
+        stop("Invalid Oauth credentials", call. = FALSE)
+    if (is.null(rev)) {
+        stop("You need to specify a revision number to restore a file \n",
+            call. = F)
+    }
+    # List should contain path and revision number
+    # 1. Check revision to make sure it exists.
 }
 
 # List of Issues
