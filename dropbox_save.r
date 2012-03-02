92b26ff1b6141cd2b2b4da40a4a38dfee8e6bd40
Karthik Ram <karthik.ram@gmail.com>
2012-03-01 17:50:44 -0800

    Minor formatting

--- dropbox_save.r
+++ dropbox_save.r
@@ -18,10 +18,8 @@ dropbox_save <- function(cred, path, file) {
     content = "This is simple content"
     input = RCurl:::uploadFunctionHandler(content, TRUE)
     trace(input)
-    # Below crashes R64
+    # Below crashes R64.app
     xx = cred$OAuthRequest("https://api-content.dropbox.com/1/files_put/dropbox/up",
         , "POST", upload = TRUE, readdata = input, infilesize = nchar(content) -
             3L, verbose = TRUE)
 }
-
-# Copy API instructions below for reference.
