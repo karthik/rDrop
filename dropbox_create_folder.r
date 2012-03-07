62420a7374bcbd9d45294d510eeead2d5eadb597
Karthik Ram <karthik.ram@gmail.com>
2012-03-06 17:05:23 -0800

    Added error handler for folder exists

--- dropbox_create_folder.r
+++ dropbox_create_folder.r
@@ -19,6 +19,9 @@ dropbox_create_folder <- function(cred, folder_name) {
     # if(!is.dropbox.dir(folder_name)) {
     # do the stuff below
     # }
+    # else {
+    # stop("Folder already exists \n", call.=FALSE)
+    # }
     dir_metadata <- fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/fileops/create_folder/",
         list(root = "dropbox", path = folder_name)))
     cat("Folder successfully created at", dir_metadata$root, dir_metadata$path,
