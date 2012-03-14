a2cd8c805c8e7ed0b0365b10b1cee6abbfdc1497
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 17:11:23 -0700

    Fixed major issues with search. Now term includes path to search.

--- dropbox_search.r
+++ dropbox_search.r
@@ -14,7 +14,6 @@
 #'@param cred An object of class ROAuth with Dropobox specific credentials.
 #'@param  query The search string. Must be at least three characters long.
 #'@param  include_deleted If this parameter is set to true, then files and folders that have been deleted will also be included in the search.
-#'@param  path The path to the folder you want to search from.
 #'@param  file_limit The maximum and default value is 1,000. No more than file_limit search results will be returned.
 #'@keywords
 #'@seealso
@@ -23,12 +22,13 @@
 #'@import RJSONIO ROAuth
 #'@export dropbox_search
 #'@examples \dontrun{
-#' results<-dropbox_search(cred,'search_term')
+#' results<-dropbox_search(cred,'file or folder name')
+#' results<-dropbox_search(cred,'/specific/path/file or folder name')
 #' Returns a small data.frame with path and is_dir
 #' results<-dropbox_search(cred,'search_term',verbose=T)
 #' Verbose results include a data.frame with columns: revision,rev,thumb_exists,bytes,modified,path,is_dir,icon,root,mime_type,size
 #'}
-dropbox_search <- function(cred, query = NULL, path = NULL, 
+dropbox_search <- function(cred, query = NULL, 
     deleted = FALSE, file_limit = 1000, is_dir = NULL, verbose = FALSE) {
     # Unable to pass on full path. So I must strip the last
     #   bit, search then match the full search path. Right?
@@ -36,28 +36,33 @@ dropbox_search <- function(cred, query = NULL, path = NULL,
         stop("Invalid or missing Dropbox credentials. ?dropbox_auth for more information.", 
             call. = FALSE)
     }
-    #Check of path is valid
+    #Check if a query was supplied
     if (is.null(query)) {
-        stop("you did not specifiy any search query")
+        stop("No term to query")
     }
-    
+    # Save the full path if supplied.
+    full_path <- query
+    query <- basename(query)
     results <- fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/search/dropbox/", 
         list(query = query, include_deleted = deleted)))
     search_results <- formatted_results <- ldply(results, data.frame)
     
-    if (!is.null(path)) {
-        full_path <- paste(path, query, sep = "")
+    # If user wanted to search for a file in a specific location.
+    if (!identical(full_path,query)) {
         search_results <- search_results[which(search_results$path == 
             full_path), ]
+        if(dim(search_results)[1]==0) {
+            stop("No results found", call.= FALSE)
+        }
     }
     
-    
+    # Test if someone is checking whether result is a directory
     if (!is.null(is_dir)) {
-        if (is_dir) {
+        if (is_dir==TRUE) {
             search_results <- search_results[search_results$is_dir, 
                 ]
         }
-        if (!is_dir) {
+        if (is_dir==FALSE) {
             search_results <- search_results[!search_results$is_dir, 
                 ]
         }
@@ -67,8 +72,7 @@ dropbox_search <- function(cred, query = NULL, path = NULL,
     small_results <- data.frame(path = search_results$path, is_dir = search_results$is_dir)
     
     if (empty(search_results)) {
-        search_results <- NULL
-        return(search_results)
+        stop("No results found", call.= FALSE)
     }
     if (!verbose & !empty(small_results)) {
         return(small_results)
