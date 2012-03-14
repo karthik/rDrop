527df2006737ef953d159341d68a67bd21de6751
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 21:34:58 -0700

    Minor edit: formatting.

--- dropbox_get.r
+++ dropbox_get.r
@@ -1,6 +1,5 @@
 # Status: Not currently working. Works but gives me junk
 #   data (real data but badly formatted)
-
 #'Downloads a file from your Dropbox
 #'
 #'@param cred Specifies an object of class ROAuth with Dropobox specific credentials.
@@ -18,22 +17,24 @@ dropbox_get <- function(cred, file_to_get) {
     if (!is.dropbox.cred(cred)) {
         stop("Invalid Oauth credentials", call. = FALSE)
     }
-
-   if (!(exists.in.dropbox(cred, path = file_to_get, is_dir = FALSE))) {
+    if (!(exists.in.dropbox(cred, path = file_to_get, is_dir = FALSE))) {
         stop("File or folder does not exist", call. = FALSE)
     }
-
- downloaded_file <- cred$OAuthRequest("https://api-content.dropbox.com/1/files/", 
+    downloaded_file <- cred$OAuthRequest("https://api-content.dropbox.com/1/files/", 
         list(root = "dropbox", path = file_to_get), "GET")
-} 
-# API documentation: https://www.dropbox.com/developers/reference/api#files-GET
-
+}
+# API documentation:
+#
+#
+#
+#   https://www.dropbox.com/developers/reference/api#files-GET
 # Notes
 # ------------------------------------------------
 # Should be limited to text or csv files.
 # No pdfs, or images, and stuff. Right?
-
 # # Errors
 # Warning message:
-# In grepl("\\\\u[0-9]", str) : input string 1 is invalid in this locale
-# Calls: <Anonymous> ... getForm -> getURLContent -> <Anonymous> -> encode -> grepl
+# In grepl('\\\\u[0-9]', str) : input string 1 is
+#   invalid in this locale
+# Calls: <Anonymous> ... getForm -> getURLContent ->
+#   <Anonymous> -> encode -> grepl 
