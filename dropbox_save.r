3a3dc0af25b26962724aad43e00fde027482b4db
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 21:36:27 -0700

    Minor edit: formatting.

--- dropbox_save.r
+++ dropbox_save.r
@@ -1,5 +1,4 @@
 #Status: DOES NOT WORK YET
-
 #' Function to save an object from R into Dropbox
 #'
 #'@param cred Specifies an object of class ROAuth with Dropobox specific credentials.
@@ -12,13 +11,13 @@
 #'@export
 #'@import RJSONIO ROAuth
 #'@examples \dontrun{
-#' dropbox_save(robject, file="filename")
+#' dropbox_save(robject, file='filename')
 #'}
 dropbox_save <- function(cred, path, file) {
     if (!is.dropbox.cred(cred)) {
         stop("Invalid Oauth credentials", call. = FALSE)
     }
-    df <- data.frame(x=1:10, y=1:10)
+    df <- data.frame(x = 1:10, y = 1:10)
     content <- "This is simple content"
     inputz <- RCurl:::uploadFunctionHandler(df, TRUE)
     trace(inputz)
@@ -26,10 +25,22 @@ dropbox_save <- function(cred, path, file) {
     xx <- cred$OAuthRequest("https://api-content.dropbox.com/1/files_put/dropbox/", 
         , "POST", upload = TRUE, readdata = inputz, infilesize = nchar(content) - 
             3L, verbose = TRUE)
-} 
-# API documentation: GET: https://www.dropbox.com/developers/reference/api#files-GET
-# POST: https://www.dropbox.com/developers/reference/api#files-POST
-
+}
+# API documentation: GET:
+#
+#
+#
+#   https://www.dropbox.com/developers/reference/api#files-GET
+# POST:
+#
+#
+#
+#   https://www.dropbox.com/developers/reference/api#files-POST
 # Testing
 # df <- data.frame(x=1:10, y=rnorm(10))
-# x <- cred$OAuthRequest("https://api-content.dropbox.com/1/files_put/dropbox/",list(file=df, filename="file.rdata", mime_type="text/csv"), "POST")
+# x <-
+#
+#
+#
+#   cred$OAuthRequest('https://api-content.dropbox.com/1/files_put/dropbox/',list(file=df,
+#   filename='file.rdata', mime_type='text/csv'), 'POST')
