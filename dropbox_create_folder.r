7acb25a69b88fb0e8a419db76d8b7a3e87e90459
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 19:54:28 -0700

    works fine.

--- dropbox_create_folder.r
+++ dropbox_create_folder.r
@@ -34,13 +34,13 @@ dropbox_create_folder <- function(cred, folder_name = NULL) {
                 1)
         }
     # Check for duplicates.
-    if (!(exists.in.dropbox(cred, folder_name, is_dir = TRUE))) {
+    if ((exists.in.dropbox(cred, folder_name, is_dir = TRUE))) {
         stop("Folder already exists", call. = FALSE)
     }
     
     # Now create the folder.
     dir_metadata <- fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/fileops/create_folder/", 
-        list(root = "dropbox", folder_name = folder_name)))
+        list(root = "dropbox", path = folder_name), "POST"))
     location <- paste(dir_metadata$root, dir_metadata$path, sep = "")
     cat("Folder successfully created at", location, "on", dir_metadata$modified, 
         "\n")
