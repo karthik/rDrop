9851d85ff4fe670ab15aa0697958e3e6ab919d0f
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 21:34:32 -0700

    Minor edit: formatting.

--- dropbox_share.r
+++ dropbox_share.r
@@ -1,6 +1,6 @@
 #' Creates and returns a shareable link to files or folders.
-#' 
-#' Returns a URL to a zipped copy of a folder or a direct link in  
+#'
+#' Returns a URL to a zipped copy of a folder or a direct link in
 #' case input is a file. Also returns a date when link will expire.
 #'@param cred An object of class ROAuth with Dropobox specific credentials.
 #'@param file Specifies the path to the file or folder you want a shareable link to.
@@ -32,5 +32,6 @@ dropbox_share <- function(cred, file = NULL) {
     res$url <- result[[1]]
     res$expires <- result[[2]]
     return(res)
-}  
-# API documentation: https://www.dropbox.com/developers/reference/api#shares
+}
+# API documentation:
+#   https://www.dropbox.com/developers/reference/api#shares
