291ed492da44facbe6f03e5d3ba3a4fd1d7c1fcd
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 19:01:46 -0700

    tried to fix errors but had to stop.

--- dropbox_create_folder.r
+++ dropbox_create_folder.r
@@ -15,8 +15,7 @@
 #'@examples \dontrun{
 #'
 #'}
-dropbox_create_folder <- function(cred, folder_name = NULL, 
-    path = NULL) {
+dropbox_create_folder <- function(cred, folder_name = NULL) {
     if (!is.dropbox.cred(cred)) {
         stop("Invalid or missing Dropbox credentials. ?dropbox_auth for more information.", 
             call. = FALSE)
@@ -27,32 +26,21 @@ dropbox_create_folder <- function(cred, folder_name = NULL,
     }
     # assuming someone wants to create this inside a sub-folder
     #   and not the dropbox root
-    if (!is.null(path)) {
-        if (grepl("/$", path)) {
-            path <- str_sub(path, end = str_length(path) - 1)
-        }
-        if (grepl("^/", path)) {
-            path <- str_sub(path, start = 2)
+        if (grepl("^/", folder_name)) {
+            folder_name <- str_sub(folder_name, start = 2)
         }
         if (grepl("/$", folder_name)) {
             folder_name <- str_sub(folder_name, end = str_length(folder_name) - 
                 1)
         }
-        if (grepl("^/", folder_name)) {
-            folder_name <- str_sub(folder_name, start = 2)
-        }
-        # Final formatted folder name with path and no extra
-        #   slashes.
-        folder_name <- paste(path, folder_name, sep = "/")
-    }
     # Check for duplicates.
-    if ((exists.in.dropbox(cred, folder_name, is_dir = TRUE))) {
+    if (!(exists.in.dropbox(cred, folder_name, is_dir = TRUE))) {
         stop("Folder already exists", call. = FALSE)
     }
     
     # Now create the folder.
     dir_metadata <- fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/fileops/create_folder/", 
-        list(root = "dropbox", path = folder_name)))
+        list(root = "dropbox", folder_name = folder_name)))
     location <- paste(dir_metadata$root, dir_metadata$path, sep = "")
     cat("Folder successfully created at", location, "on", dir_metadata$modified, 
         "\n")
