e5a581a733ec85060bc5e95cb2c2bb806ebf87a8
Karthik Ram <karthik.ram@gmail.com>
2012-03-23 12:54:51 -0700

    removed internet connection check for time being.

--- R/dropbox_error_handlers.r
+++ R/dropbox_error_handlers.r
@@ -145,19 +145,3 @@ is.valid.revision <- function(cred, path = NULL, revision = NULL) {
 #   append it.
 # Checks revision number for a file in dropbox and returns
 #   a logical yes/no.
-
-#'Checks for valid internet connection
-#' @return logical TRUE/FALSE
-#' @examples \dontrun{
-#' havingIP()
-#'}
-#' @export
-havingIP <- function() {
-  if (.Platform$OS.type == "windows") {
-    ipmessage <- system("ipconfig", intern = TRUE)
-  } else {
-    ipmessage <- system("ifconfig", intern = TRUE)
-  }
-  validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-  any(grep(validIP, ipmessage))
-}
