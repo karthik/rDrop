14a711eb31db95f54cfee74d82d1e4895158f57e
Karthik Ram <karthik.ram@gmail.com>
2012-03-24 12:33:54 -0700

    Moved key checking to funtion arg using getOption

--- R/dropbox_auth.r
+++ R/dropbox_auth.r
@@ -18,21 +18,17 @@
 #' dropbox_token <- dropbox_auth('consumey_key', 'consumer_secret')
 #' save(dropbox_token, file = 'dropbox_auth.rdata')
 #'}
-dropbox_auth <- function(cKey = NULL, cSecret = NULL, 
+dropbox_auth <- function(cKey = getOption("DropboxKey", NULL), cSecret = getOption("DropboxSecret", NULL),
     verbose = FALSE) {
         #Checking to make sure you specify keys one way or
         #   another
     if (is.null(cKey) && is.null(cSecret)) {
-        cKey <- getOption("DropboxKey")
-        cSecret <- getOption("DropboxSecret")
-        if (length(cKey) == 0 | length(cSecret) == 0) {
             stop("Could not find your Dropbox keys in the function arguments or in your options. ?rDrop for more help")
-        }
     }
     reqURL <- "https://api.dropbox.com/1/oauth/request_token"
     authURL <- "https://www.dropbox.com/1/oauth/authorize"
     accessURL <- "https://api.dropbox.com/1/oauth/access_token/"
-    cred <- OAuthFactory$new(consumerKey = cKey, consumerSecret = cSecret, 
+    cred <- OAuthFactory$new(consumerKey = cKey, consumerSecret = cSecret,
         requestURL = reqURL, accessURL = accessURL, authURL = authURL)
     cat("Beginning authenticating with Dropbox... \n")
     cred$handshake(post = FALSE)
@@ -44,7 +40,7 @@ dropbox_auth <- function(cKey = NULL, cSecret = NULL,
     if (FALSE) {
         cred$OAuthRequest("https://api.dropbox.com/1/account/info")
         cred$OAuthRequest("https://api-content.dropbox.com/1/files/dropbox/foo")
-        cred$OAuthRequest("https://api-content.dropbox.com/1/files/dropbox/foo", 
+        cred$OAuthRequest("https://api-content.dropbox.com/1/files/dropbox/foo",
             httpheader = c(Range = "bytes=30-70"), verbose = TRUE)
     }
     return(cred)
@@ -54,4 +50,4 @@ dropbox_auth <- function(cKey = NULL, cSecret = NULL,
 #
 #
 #
-#   https://www.dropbox.com/developers/reference/api#request-token  
+#   https://www.dropbox.com/developers/reference/api#request-token
