1bdf12b836658793746c9555dc1f9bf56a0b0b48
Karthik Ram <karthik.ram@gmail.com>
2012-03-26 09:29:15 -0700

    Changed auth from ref class to S4

--- R/dropbox_acc_info.r
+++ R/dropbox_acc_info.r
@@ -10,12 +10,12 @@
 #' dropbox_acc_info(cred)
 #'}
 dropbox_acc_info <- function(cred) {
-    if (!is.dropbox.cred(cred)) {
+    if (class(cred) != "DropboxCredentials" | missing(cred)) {
         stop("Invalid or missing Dropbox credentials. ?dropbox_auth for more information.", 
             call. = FALSE)
     }
-    status <- fromJSON(cred$OAuthRequest("https://api.dropbox.com/1/account/info"))
-    return(status)
+    info <- fromJSON(OAuthRequest(cred, "https://api.dropbox.com/1/account/info"))
+    return(info)
 }
 # API documentation:
 #
