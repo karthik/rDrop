85821312df240d8dadbbf3d0ff79fcbc04e735ac
Karthik Ram <karthik.ram@gmail.com>
2012-03-25 19:55:50 -0700

    Updated roxygen documentation.

--- R/dropbox_get.r
+++ R/dropbox_get.r
@@ -1,7 +1,6 @@
-# Status: Not currently working. Works but gives me junk
-#   data (real data but badly formatted)
 #'Downloads a file from your Dropbox
 #'
+#' Currently the function does not provide much support other than retrieving the contents of whatever Dropbox file you specify. Use \code{TextConnection} to process data files for the time being.
 #' @param cred Specifies an object of class ROAuth with Dropobox specific credentials.
 #' @param  file_to_get Specifies the path to the file you want to retrieve.
 #' @return file
@@ -17,11 +16,11 @@ dropbox_get <- function(cred, file_to_get) {
     if (!(exists.in.dropbox(cred, path = file_to_get, is_dir = FALSE))) {
         stop("File or folder does not exist", call. = FALSE)
     }
-    downloaded_file <- cred$OAuthRequest("https://api-content.dropbox.com/1/files/", 
+    downloaded_file <- cred$OAuthRequest("https://api-content.dropbox.com/1/files/",
         list(root = "dropbox", path = file_to_get), "GET")
 }
 # API documentation:
 #
 #
 #
-#   https://www.dropbox.com/developers/reference/api#files-GET 
+#   https://www.dropbox.com/developers/reference/api#files-GET
