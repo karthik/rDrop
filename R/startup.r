070feafbbd0d10305bd2efbba42e3a40df3e2aca
Karthik Ram <karthik.ram@gmail.com>
2012-03-27 12:53:36 -0700

    added startup message.

---
+++ R/startup.r
@@ -0,0 +1,9 @@
+.onAttach <- function(...) {
+  if (stats::runif(1) > 0.1) return()
+  tips <- c(
+    "New to rDrop? Start with ?dropbox_auth()\n",
+    "Use suppressPackageStartupMessages to eliminate package startup messages."
+  )
+  tip <- sample(tips, 1)
+  packageStartupMessage(tip)
+}
