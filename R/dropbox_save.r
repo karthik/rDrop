7582e8acb1190478aa8013b412a9059688527a2a
Karthik Ram <karthik.ram@gmail.com>
2012-03-27 14:03:16 -0700

    Dropbox save now works fine.

--- R/dropbox_save.r
+++ R/dropbox_save.r
@@ -2,36 +2,52 @@
 #'
 #' This function currently does not work.
 #' @param cred Specifies an object of class ROAuth with Dropobox specific credentials.
-#' @param content The file contents to be uploaded. Requires a multipart upload (multipart/form-data), and the filename parameter of this field should be set to the desired destination filename. While signing this request for OAuth, the file parameter should be set to the destination filename, and then switched to the file contents when preparing the multipart request.
+#' @param list List of objects from the current R environment that needs to be saved into dropbox
+#' @param file Required filename. No extenstion needs to be supplied. If you provide one, it will be stripped and replace with rda.
+#' @param envir optional. Defaults to parent environment.
+#' @param precheck internal use. Checks to make sure all objects are in the parent environment.
 #' @param curl If using in a loop, call getCurlHandle() first and pass
 #'  the returned value in here (avoids unnecessary footprint)
-#' @param ... optional additional curl options (debugging tools mostly)#' @param file Name of file to be saved. If blank, the name of the R object will be used.
+#' @param ... optional additional curl options (debugging tools mostly)
 #' @export
 #' @import RJSONIO ROAuth
 #' @examples \dontrun{
 #' dropbox_save(robject, file='filename')
 #'}
-dropbox_save <- function(cred, content, file = NULL, curl=getCurlHandle(), ...) {
+dropbox_save <- function(cred,..., list = character(), file = stop("'file' must be specified"), envir = parent.frame(), precheck=TRUE,curl=getCurlHandle()) {
+
     if (class(cred) != "DropboxCredentials" | missing(cred)) {
         stop("Invalid or missing Dropbox credentials. ?dropbox_auth for more information.")
     }
-    if (is.null(content)) {
-        stop("Nothing to save", call. = FALSE)
-    }
-    if (is.null(file)) {
-        filename <- paste(deparse(substitute(content)), ".rda",
-            sep = "")
-    }
-    if(!is.null(file)) {
-        filename <- paste(file, ".rda", sep = "")
-    }
-      destination <- paste("https://api-content.dropbox.com/1/files_put/dropbox/",
-        filename, sep = "")
-    input <- RCurl:::uploadFunctionHandler(content, TRUE)
-    xx <-OAuthRequest(cred, "https://api-content.dropbox.com/1/files_put/dropbox/Public/up.rda",
-        , "PUT", .opts = list(readfunction = input, infilesize = nchar(content),
-            verbose = TRUE))
-    return(fromJSON(xx))
+    names <- as.character(substitute(list(...)))[-1L]
+    list <- c(list, names)
+            if (precheck) {
+            ok <- unlist(lapply(list, exists, envir = envir))
+            if (!all(ok)) {
+                n <- sum(!ok)
+                stop(sprintf(ngettext(n, "object %s not found",
+                  "objects %s not found"), paste(sQuote(list[!ok]),
+                  collapse = ", ")), domain = NA)
+            }
+        }
+         if (is.character(file)) {
+            if (!nzchar(file))
+                stop("'file' must be non-empty string")
+            }
+ filename <- paste(str_trim(str_extract(file, "[^.]*")), ".rda", sep = "")
+ url <- paste("https://api-content.dropbox.com/1/files_put/dropbox/",filename, sep="")
+
+con <- rawConnection(raw(), "w")
+serialize(list(a = 1:10, b = letters), con)
+    z <- rawConnectionValue(con)
+    input <- RCurl:::uploadFunctionHandler(z, TRUE)
+    xx <- OAuthRequest(cred, url,
+        , "PUT", upload = TRUE, readfunction = input, infilesize = nchar(z),
+        verbose = FALSE, httpheader = c(`Content-Type` = "application/binary"))
+    close(con)
+  fromJSON(xx)
 }
 # API documentation: GET:
 #   https://www.dropbox.com/developers/reference/api#files-GET
+
+
