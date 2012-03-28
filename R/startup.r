76ea3a400f7ad5f65bf14e8bd52514beb7059830
Karthik Ram <karthik.ram@gmail.com>
2012-03-27 20:11:43 -0700

    updated documentation and startup message.

--- R/startup.r
+++ R/startup.r
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-packageStartupMessage("\n\n New to rDrop? Look up ?dropbox_auth() for setting up an app. Use suppressMessages() to suppress this message in future\n")
+packageStartupMessage("\n\n New to rDrop? Look up ?dropbox_auth() for setting up an app. Use suppressPackageStartupMessages() to suppress this message in future\n")
 }
