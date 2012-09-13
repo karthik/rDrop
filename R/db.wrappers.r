8df7658be7e7147fb4a80e4bb7b42efb62ee1e6a
Karthik Ram <karthik.ram@gmail.com>
2012-09-13 16:37:22 -0700

    updated broken scripts

--- R/db.wrappers.r
+++ R/db.wrappers.r
@@ -3,20 +3,15 @@
 #' This function is a simple wrapper around \code{\link{dropbox_get}} for csv files.
 #' @param dropbox_credentials Specifies an object of class DropboxCredentials with Dropobox specific credentials.
 #' @param  file_to_get Specifies the path to the file you want to retrieve. Path must be relative to \code{Dropbox root}.
-#' @param header Default is \code{TRUE}. Set to \code{FALSE} if your csv file does not have a header row.
 #' @export
 #' @seealso dropbox_get
 #' @return data.frame
 #' @examples \dontrun{
 #' my_data <- db.read.csv(db_cred, 'data.csv', header = TRUE)
 #'}
-db.read.csv <- function(dropbox_credentials, file_to_get,
-    header = TRUE) {
+db.read.csv <- function(dropbox_credentials, file_to_get) {
     file <- dropbox_get(dropbox_credentials, file_to_get)
-    data <- read.csv(textConnection(file), header)
-    data2 <- data.frame(data[,-1])
-    names(data2) <- names(data)[-1]
-    return(data2)
+    return(unserialize(file))
 }
 
 
