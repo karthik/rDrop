cbb09fdcc958dbb30383d8162e31a4c86206ed85
Duncan Temple Lang <duncan@r-project.org>
2013-04-07 12:32:18 -0700

    start of interrupt error

--- R/errors.R
+++ R/errors.R
@@ -6,10 +6,17 @@ function()
 }
 
 raiseError <-
-function(msg, class)
+function(msg, class, ...)
 {
-  e <- structure(list(message = msg),
+  e <- structure(list(message = msg, ...),
                  class = c(class, "rDropError", "simpleError", "error", "condition"))
    
    stop(e, call = NULL) #  , call.= FALSE)
+}
+
+
+interruptedUpload <-
+function(upload_id, offset)
+{
+  raiseError("", "UploadInterrupted", upload_id = upload_id, offset = offset)
 }
